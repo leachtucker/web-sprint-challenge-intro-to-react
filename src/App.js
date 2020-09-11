a28e2af1dfc107c54199b77caeec3908ab282f92
Tucker Leach <leachtucker@gmail.com>
2020-09-11 10:29:12 -0700

    Completed cards for each character. Finishing up modals

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import fetchCharacters from './data/fetch.js';
 import Character from './components/Character.js';
+import Grid from '@material-ui/core/Grid';
 
 import './App.css';
 
@@ -20,9 +21,27 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  if (!characters) {
+    return (
+      <div className="App">
+        <h1 className="Header">Characters</h1>
+        <div className="container">
+          <h2>LOADING...</h2>
+        </div>
+    </div>
+    );
+  }
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Grid container className="container">
+        {characters.map(charr => {
+          return (
+            <Character props={charr} key={charr.id} />
+          );
+        })}
+      </Grid>
     </div>
   );
 }
